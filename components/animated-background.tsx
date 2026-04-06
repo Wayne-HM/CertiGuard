3024b3c4dfeaae7eec1c9baae3642d133e68249b
Syed <knownasperfect@gmail.com>
2026-04-07 01:30:25 +0530

    Massive frontend scroll performance improvements

--- components/animated-background.tsx
+++ components/animated-background.tsx
@@ -28,14 +28,6 @@ export function AnimatedBackground() {
       />
       
       <div 
-        className="fixed inset-0 pointer-events-none opacity-[0.012]"
-        style={{ 
-          zIndex: 2,
-          backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noise)'/%3E%3C/svg%3E")`,
-        }}
-      />
-      
-      <div 
         className="fixed inset-0 pointer-events-none"
         style={{ 
           zIndex: 2,
